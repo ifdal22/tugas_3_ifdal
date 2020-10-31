b33a67ef25bb042335a042cc5349ce3a5fc55271
ifdal22 <73428516+ifdal22@users.noreply.github.com>
2020-10-31 22:05:02 +0700

    Add files via upload

---
+++ tugas3_ifdal/HtmlPostData/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	fn "tugas3_ifdal/HtmlPostData/function"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/northwind")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	r := mux.NewRouter()
+
+	http.HandleFunc("/", fn.RouteIndexGet)
+	http.HandleFunc("/process", fn.RouteSubmitPost)
+	r.HandleFunc("/process", fn.RouteSubmitPost).Methods("POST")
+
+	fmt.Println("server started at localhost:9090")
+	http.ListenAndServe(":9090", r)
+
+}
